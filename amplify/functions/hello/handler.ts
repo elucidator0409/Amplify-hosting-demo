888e254611852dceabb3a865447db86da637455a
tony <tranquockhanhdhsp99@gmail.com>
2025-05-23 17:26:04 +0700

    Add example for Amplify function

--- amplify/functions/hello/handler.ts
+++ amplify/functions/hello/handler.ts
@@ -1,11 +1,10 @@
 import type { Schema } from "../../data/resource"
-
+import { env } from "$amplify/env/sayHello"
 
 type handlerType = Schema['sayHello']['functionHandler']
 
-
-
 export const handler: handlerType = async(event) => {
+    const tableName = env.TABLE_NAME;
     const { name } = event.arguments
-    return `Hello, ${name}!`
+    return `Hello, ${name}, table name: ${tableName}!`
 }
