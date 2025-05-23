8f9d9e9e35d64023dc5ec5018129fc8ab7c91149
tony <tranquockhanhdhsp99@gmail.com>
2025-05-23 13:57:19 +0700

    Amplify function

---
+++ amplify/functions/hello/handler.ts
@@ -0,0 +1,11 @@
+import type { Schema } from "../../data/resource"
+
+
+type handlerType = Schema['sayHello']['functionHandler']
+
+
+
+export const handler: handlerType = async(event) => {
+    const { name } = event.arguments
+    return `Hello, ${name}!`
+}
